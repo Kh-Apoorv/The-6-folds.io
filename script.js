047aeabe9f3959e3438bf039d1f2251b270429c3
Apoorv Khandelwal <73796836+Kh-Apoorv@users.noreply.github.com>
2023-07-06 03:25:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Interior Desingers", "Architects", "Renovators"],
+        strings: ["Interior Designers", "Architects", "Renovators"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Interior Desingers", "Architects", "Renovators"],
+        strings: ["Interior Designers", "Architects", "Renovators"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
